6843b36a884b02cadfe6c033cb84a793f2598937
Guilhermedelauro <guidelauro@gmail.com>
2026-03-25 11:51:24 -0300

    Código de gráfico de barras votos por candidato

---
+++ analise.R
@@ -0,0 +1,10 @@
+
+library(tidyverse)
+
+eleicao <- read.csv("C:\\Users\\guide\\Downloads\\eleicoes_2022.csv")
+glimpse(eleicao)
+
+grafico_barras <- eleicao %>%
+  group_by(candidato)%>%
+  ggplot(aes(x=candidato, y=votos)) +geom_col() 
+print(grafico_barras)
